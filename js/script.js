8fecd625e1fa92848528918be2931bfea790af40
Saksham Singh <geekysaksham27@gmail.com>
2019-12-17 04:10:22 +0530

    Added scroll fade animation

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Smmoth scroll
+var scroll = new SmoothScroll('a[href*="#"]');
+
+// Scroll Reveal
+ScrollReveal().reveal('.section-about', {delay: 300});
+ScrollReveal().reveal('.skills', {delay: 300});
+
+ScrollReveal().reveal('.section-portfolio', {delay:300});
+ScrollReveal().reveal('.section-portfolio--porfolio-container', {delay:300});
+ScrollReveal().reveal('.section-contact', {delay:200});
