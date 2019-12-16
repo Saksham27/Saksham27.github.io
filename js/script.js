fbf54a3e8e158ab43ee0e9991b66e274654caf51
Saksham Singh <geekysaksham27@gmail.com>
2019-12-17 04:54:06 +0530

    Fixing bugs

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 var scroll = new SmoothScroll('a[href*="#"]');
 
 // Scroll Reveal
-ScrollReveal().reveal('.section-about', {delay: 200});
-ScrollReveal().reveal('.skills', {delay: 200});
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.section-about', { delay: 200 });
+ScrollReveal().reveal('.skills', { delay: 200 });
 
-ScrollReveal().reveal('.section-portfolio', {delay:200});
-ScrollReveal().reveal('.section-portfolio--porfolio-container', {delay:200});
-ScrollReveal().reveal('.section-contact', {delay:200});
+ScrollReveal().reveal('.section-portfolio', { delay: 200 });
+ScrollReveal().reveal('.section-portfolio--porfolio-container', { delay: 200 });
+ScrollReveal().reveal('.section-contact', { delay: 200 });
