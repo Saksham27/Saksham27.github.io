1f91d8d18a6af2c37e407bebfda3a7a59962a3dd
Saksham Singh <geekysaksham27@gmail.com>
2019-12-17 04:35:10 +0530

    implemented browser caching

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 var scroll = new SmoothScroll('a[href*="#"]');
 
 // Scroll Reveal
-ScrollReveal().reveal('.section-about', {delay: 300});
-ScrollReveal().reveal('.skills', {delay: 300});
+ScrollReveal().reveal('.section-about', {delay: 200});
+ScrollReveal().reveal('.skills', {delay: 200});
 
-ScrollReveal().reveal('.section-portfolio', {delay:300});
-ScrollReveal().reveal('.section-portfolio--porfolio-container', {delay:300});
+ScrollReveal().reveal('.section-portfolio', {delay:200});
+ScrollReveal().reveal('.section-portfolio--porfolio-container', {delay:200});
 ScrollReveal().reveal('.section-contact', {delay:200});
