6155e110fe8c30d88dc34344df22658081f807c3
Saksham Singh <geekysaksham27@gmail.com>
2019-12-17 06:53:01 +0530

    fixing bugs and typo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Smmoth scroll
 var scroll = new SmoothScroll('a[href*="#"]');
 
+$('#spyOnThis').scrollspy();
 // Scroll Reveal
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.section-about', { delay: 200 });
@@ -9,3 +10,6 @@ ScrollReveal().reveal('.skills', { delay: 200 });
 ScrollReveal().reveal('.section-portfolio', { delay: 200 });
 ScrollReveal().reveal('.section-portfolio--porfolio-container', { delay: 200 });
 ScrollReveal().reveal('.section-contact', { delay: 200 });
+
+// PWA
+
